b18826620eb79bbd0734eb98bff5845f3a257645
Alistair Quinn <Alistair Quinn>
2018-03-12 16:00:32 +0000

    added joke route

--- lab13/server.js
+++ lab13/server.js
@@ -9,4 +9,10 @@ app.get('/test', function(req,res){
     res.send("This is route 2");
 });
 
+app.get('/joke', function(req,res){
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    var randomJoke = oneLinerJoke.getRandomJoke();
+    res.end(randomJoke.body);
+});
+
 app.listen(8080);
