6856a4fe2ad2720d6d9f0b9e36c3960ffc76e388
Alistair Quinn <Alistair Quinn>
2018-03-13 12:37:46 +0000

    fix

--- lab14/server.js
+++ lab14/server.js
@@ -4,9 +4,9 @@ var Twitter = require('twitter');
 
 var client = new Twitter({
     consumer_key: '	6Ft1pIy6lvQehU54D4nDdU0gC',
-    consumer_secret: '	jOJbnxQ2oFfm5XrorWqEcdIHJm1c0doOSpmBFrkuK7NkC16Oui',
-    access_token_key: '	853735939730010112-fl4BtAY41BN2g7uU6HBNQUKCeH5q8BI',
-    access_token_secret: '	hpdwNNHHBjLQI3HqVXhuYH3iAIngdR0feHcSyJISteymK'
+    consumer_secret: 'jOJbnxQ2oFfm5XrorWqEcdIHJm1c0doOSpmBFrkuK7NkC16Oui',
+    access_token_key: '853735939730010112-fl4BtAY41BN2g7uU6HBNQUKCeH5q8BI',
+    access_token_secret: 'hpdwNNHHBjLQI3HqVXhuYH3iAIngdR0feHcSyJISteymK'
 });
 
 var params = {screen_name: 'nodejs'};
@@ -17,6 +17,10 @@ app.get('/', function(req,res){
     client.get('statuses/user_timeline', params, function(error, tweets, response){
         if (!error){
             console.log(tweets);
+        }
+        else {
+            console.log(error);
+            console.log(response);
         }
     });
     res.send("Hello world! by express");
